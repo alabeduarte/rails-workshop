b6f0f6d08835c2860f906f6212a294b505ffd3ff
Alabê Duarte <aduarte@thoughtworks.com>
2014-05-20 18:36:26 -0300

    [Failing] Implemented listing of attendees

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -14,6 +14,10 @@ class AttendeesController < ApplicationController
     @attendee = Attendee.find(params[:id])
   end
 
+  def index
+    @attendees = Attendee.all
+  end
+
   private
   def attendee_params
     params.require(:attendee).permit(:name)
