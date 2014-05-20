51de983d18cd51f8d4ea7831d56e411b43bbb307
Alabê Duarte <aduarte@thoughtworks.com>
2014-05-20 17:43:45 -0300

    Created unit tests for the attendees controller

--- spec/controllers/attendees_controller_spec.rb
+++ spec/controllers/attendees_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe AttendeesController, :type => :controller do
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
 
+    it "renders the index template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
 end
--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe Attendee, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
